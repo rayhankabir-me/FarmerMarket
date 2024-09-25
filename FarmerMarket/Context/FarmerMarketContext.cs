541dde23fa103c9ffc22378afa797e496e9fae66
WTMrittika <21-45108-2@student.aiub.edu>
2024-09-25 23:23:34 +0600

    order table

--- FarmerMarket/Context/FarmerMarketContext.cs
+++ FarmerMarket/Context/FarmerMarketContext.cs
@@ -27,6 +27,7 @@ namespace FarmerMarket.Context
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
 
     }
 
---
+++ FarmerMarket/Models/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FarmerMarket.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "0:yyyy-MM-dd", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> OrderDate { get; set; }
+
+        [Required]
+        public double TotalPrice { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string PaymentMethod { get; set; }
+
+        [Required]
+        public  string PayementStatus { get; set; }
+
+        [Required]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        public int OrderStatus { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public Product Product { get; set; }
+        public User User { get; set; }
+    }
+
+}
